ffa8c00755b3f7b06a1fe5942b02d844deb6a455
arifbd2221 <mohidulhoque216@gmail.com>
2021-08-05 15:38:12 +0600

    Initial commit

---
+++ app/src/main/java/com/goama/admob/MainActivity.java
@@ -0,0 +1,197 @@
+package com.goama.admob;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
+import com.google.android.gms.ads.admanager.AdManagerAdRequest;
+import com.google.android.gms.ads.admanager.AdManagerAdView;
+import com.google.android.gms.ads.admanager.AdManagerInterstitialAd;
+import com.google.android.gms.ads.admanager.AdManagerInterstitialAdLoadCallback;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    private AdManagerAdView adView;
+    private AdManagerInterstitialAd interstitialAd;
+    private boolean adIsLoading;
+    private static final String interstitialAdunit = "ca-app-pub-7572720877415603/6353486639";
+    String TAG = "MainActivity";
+    Context context;
+    WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //adView = findViewById(R.id.ad_view);
+        context = this;
+
+        mWebView = (WebView) findViewById(R.id.webview);
+        initWebView();
+
+        String ENROLLMENT_URL = "https://dev.go-games.gg/tournament?app=bkash&country=bd&userid=shamee&name=Shamee&profile_pic=https%3A%2F%2Fi.pravatar.cc%2F150%3Fimg%3D3";
+        //mWebView.loadUrl(ENROLLMENT_URL);
+
+        mWebView.post(new Runnable() {
+            @Override
+            public void run() {
+                mWebView.loadUrl(ENROLLMENT_URL);
+            }
+        });
+
+
+        MobileAds.setRequestConfiguration(
+                new RequestConfiguration.Builder()
+                        .build());
+
+        // Initialize the Mobile Ads SDK.
+        MobileAds.initialize(
+                this,
+                new OnInitializationCompleteListener() {
+                    @Override
+                    public void onInitializationComplete(InitializationStatus status) {}
+                });
+
+        // Start loading the ad in the background.
+        loadAd();
+        //adView.loadAd(adRequest);
+    }
+
+    @Override
+    public void onPause() {
+//        if (adView != null) {
+//            adView.pause();
+//        }
+        super.onPause();
+    }
+
+    /** Called when returning to the activity */
+    @Override
+    public void onResume() {
+        super.onResume();
+//        if (adView != null) {
+//            adView.resume();
+//        }
+    }
+
+    /** Called before the activity is destroyed */
+    @Override
+    public void onDestroy() {
+//        if (adView != null) {
+//            adView.destroy();
+//        }
+        super.onDestroy();
+    }
+
+    @SuppressLint({ "SetJavaScriptEnabled" })
+    private void initWebView() {
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.addJavascriptInterface(new WebviewInterface(), "Interface");
+    }
+
+    public class WebviewInterface {
+        @JavascriptInterface
+        public void javaMethod(String val) {
+            Log.i(TAG, val);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (interstitialAd != null) {
+                        interstitialAd.show(MainActivity.this);
+                    }
+                }
+            });
+        }
+    }
+
+
+
+    public void loadAd() {
+        AdManagerAdRequest adRequest = new AdManagerAdRequest.Builder().build();
+        AdManagerInterstitialAd.load(
+                this,
+                interstitialAdunit,
+                adRequest,
+                new AdManagerInterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull AdManagerInterstitialAd interstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        MainActivity.this.interstitialAd = interstitialAd;
+                        Log.i(TAG, "onAdLoaded");
+                        //Toast.makeText(MainActivity.this, "onAdLoaded()", Toast.LENGTH_SHORT).show();
+                        interstitialAd.setFullScreenContentCallback(
+                                new FullScreenContentCallback() {
+                                    @Override
+                                    public void onAdDismissedFullScreenContent() {
+                                        // Called when fullscreen content is dismissed.
+                                        Log.d("TAG", "The ad was dismissed.");
+                                        // Make sure to set your reference to null so you don't
+                                        // show it a second time.
+                                        MainActivity.this.interstitialAd = null;
+                                    }
+
+                                    @Override
+                                    public void onAdFailedToShowFullScreenContent(AdError adError) {
+                                        // Called when fullscreen content failed to show.
+                                        Log.d("TAG", "The ad failed to show.");
+                                        // Make sure to set your reference to null so you don't
+                                        // show it a second time.
+                                        MainActivity.this.interstitialAd = null;
+                                    }
+
+                                    @Override
+                                    public void onAdShowedFullScreenContent() {
+                                        // Called when fullscreen content is shown.
+                                        Log.d("TAG", "The ad was shown.");
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        Log.i(TAG, loadAdError.getMessage());
+                        interstitialAd = null;
+                        adIsLoading = false;
+                        String error =
+                                String.format(
+                                        "domain: %s, code: %d, message: %s",
+                                        loadAdError.getDomain(), loadAdError.getCode(), loadAdError.getMessage());
+                        Toast.makeText(
+                                MainActivity.this, "onAdFailedToLoad() with error: " + error, Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+    }
+
+    private void showInterstitial() {
+        // Show the ad if it's ready. Otherwise toast and restart the game.
+        if (interstitialAd != null) {
+            interstitialAd.show(this);
+
+        } else {
+            Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
+
